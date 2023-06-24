6921a6b04b3db5877bf8804da4e3ea35c790fbf3
Aishbs <bsaishwarya43@gmail.com>
2023-06-24 13:32:51 +0530

    Cross-screen Navigation between Meals Screen and Category Screen

---
+++ meals_app/lib/screens/categories.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(meals: [], title: 'title'),
+      ),
+    ); //Navigator.push(context, route);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your Category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          //availableCategories.map((category) => ategoryGridItem(category: category,)).toLisy()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
