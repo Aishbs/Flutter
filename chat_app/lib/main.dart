776b6b8518ba27f44dcc6659e135908882a2efde
Aishbs <bsaishwarya43@gmail.com>
2023-07-01 11:06:27 +0530

    Logout feature

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,7 +26,20 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
