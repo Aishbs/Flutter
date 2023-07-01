f4d6edef7e0b99cd41782da65bbb4567893951e5
Aishbs <bsaishwarya43@gmail.com>
2023-07-01 08:36:45 +0530

    Chat App - Initial Commit

---
+++ chat_app/lib/screens/auth.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
